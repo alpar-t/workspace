7a933c6fb1599859932cd6179b7ce1d2440a2fd8
Alpar Torok <torokalpar@gmail.com>
2015-07-24 10:11:48 +0300

    Wraper script to bnring up opengrok in docker

--- profiles/base/bin/run_opengrok.sh
+++ profiles/base/bin/run_opengrok.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-docker run -d -v ~/devfactory_work/svn/FirewallForCode/trunk/:/src -p 80:8080 steinwaywhw/opengrok
+if [ $1 == "" ] ;  then 
+    echo "usage: $0 Path_to_Index"
+    exit 1
+fi
+docker run -d -v $1:/src -p 80:8080 steinwaywhw/opengrok
