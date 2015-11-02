ce542146643eeb4845a4dc63fb71a017718b0e68
Alpar Torok <alpar.torok@devfactory.com>
2015-11-02 11:48:48 +0200

    correct initialization scripts and layout

--- profiles/base/bin/init_i3.sh
+++ profiles/base/bin/init_i3.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
 i3-msg "workspace 1; append_layout $HOME/.i3/w1_layout.json"
-i3-msg "workspace 1; exec skype"
-i3-msg "workspace 1; exec chromium"
-i3-msg "workspace 3; append_layout $HOME/.i3/w3_layout.json"
+i3-msg "exec chromium; exec idea.sh; exec pidgin"
+sleep 2
+i3-msg "exec skype; exec hipchat"
 
