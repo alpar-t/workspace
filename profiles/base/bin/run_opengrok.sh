0dae61e02a4301a597d8c3092d91ea86f33005a5
Alpar Torok <alpar.torok@devfactory.com>
2015-11-02 11:48:04 +0200

    updated opengrok script

--- profiles/base/bin/run_opengrok.sh
+++ profiles/base/bin/run_opengrok.sh
@@ -3,4 +3,15 @@ if [ $1 == "" ] ;  then
     echo "usage: $0 Path_to_Index"
     exit 1
 fi
-docker run -d -v $1:/src -p 80:8080 steinwaywhw/opengrok
+NAME="opengrok-`basename $1`" 
+if docker ps | grep -q $NAME ; then 
+    echo $NAME allready running
+else
+    if docker ps -a | grep -q $NAME ; then
+        echo "$NAME allready exists. Starting it up."
+        docker start "$NAME"
+    else        
+        # brand new 
+        docker run --name "$NAME" -d -v $1:/src -p 80:8080 steinwaywhw/opengrok
+    fi
+fi
