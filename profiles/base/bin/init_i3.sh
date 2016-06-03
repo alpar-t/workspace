414f13c1069b4f1d1791a21c0a0791b642b9bd49
Alpar Torok <alpar.torok@devfactory.com>
2016-06-03 09:05:42 +0300

    Update to hipchat 4

--- profiles/base/bin/init_i3.sh
+++ profiles/base/bin/init_i3.sh
@@ -2,5 +2,5 @@
 i3-msg "workspace 1; append_layout $HOME/.i3/w1_layout.json"
 i3-msg "exec chromium; exec idea.sh; exec pidgin"
 sleep 2
-i3-msg "exec skype; exec hipchat"
+i3-msg "exec skype; exec HipChat4"
 
