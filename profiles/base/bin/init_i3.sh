6431ea48c1a51531c79184bb340f4a63edf5a56c
Alpar Torok <torokalpar@gmail.com>
2017-03-21 23:15:14 +0200

    multiple minor updates

--- profiles/base/bin/init_i3.sh
+++ profiles/base/bin/init_i3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 i3-msg "workspace 1; append_layout $HOME/.i3/w1_layout.json"
-i3-msg "exec chromium; exec idea.sh"
-sleep 2
-i3-msg "exec skype; exec hipchat4"
+i3-msg "exec chromium"
+i3-msg "exec skype"
+i3-msg "exec slack"
 
