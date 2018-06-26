bd1c749e93c1468635096a3bb7b199f400eb518f
Alpar Torok <torokalpar@gmail.com>
2018-06-26 09:54:13 +0300

    update i3 init script

--- profiles/base/bin/init_i3.sh
+++ profiles/base/bin/init_i3.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 i3-msg "workspace 1; append_layout $HOME/.i3/w1_layout.json"
+i3-msg "workspace 2; append_layout $HOME/.i3/w2_layout.json"
+i3-msg "workspace 1"
 i3-msg "exec google-chrome-stable"
-i3-msg "exec env LD_PRELOAD=/usr/lib/libcurl.so.3 /usr/bin/slack --disable-gpu"
+i3-msg "exec env LD_PRELOAD=/usr/lib/libcurl.so.3 /usr/bin/slack"
+i3-msg "exec intellij-idea-ultimate-edition"
 
